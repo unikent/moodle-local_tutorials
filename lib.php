b919892334c82d5cdaf9f00106c9d82513fc0416
Skylar Kelty <S.Kelty@kent.ac.uk>
2014-10-13 16:36:09 +0100

    Break it.

--- lib.php
+++ lib.php
@@ -14,4 +14,4 @@
 // You should have received a copy of the GNU General Public License
 // along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
 
-\local_tutorials\Page::on_load();
+//\local_tutorials\Page::on_load();
